fe48dcbcde07bc7dd2070266d03686caecf61055
chemberlen <7379167@gmail.com>
2018-12-04 08:40:36 +0200

    logining manage stay on registration form

--- www/start/start.py
+++ www/start/start.py
@@ -6,8 +6,3 @@ from FlaskApp.models import User, Post
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
 
-# if __name__ == '__main__':
-#     app.debug = True
-#     host = os.environ.get('IP', '0.0.0.0')
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host=host, port=port)
